166e7d3190686f558aea0ddcbd695d4ea2bc7d20
FTC20336 <ftc20336@gmail.com>
2023-02-06 17:07:23 -0600

    New constant

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner_files/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner_files/teamcode/drive/DriveConstants.java
@@ -36,9 +36,10 @@ public class DriveConstants {
      * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
      * from DriveVelocityPIDTuner.
      */
+    // getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV)
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
-            getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(19, 0, 5,
+           12.7);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -50,7 +51,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.88976; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 14.3; // in
+    public static double TRACK_WIDTH = 13.71; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -58,7 +59,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0;
+    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
     public static double kA = 0;
     public static double kStatic = 0;
 
