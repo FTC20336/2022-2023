239eaededcd5dde104268eafdecba40186efa1b7
FTC20336 <ftc20336@gmail.com>
2023-02-08 13:22:30 -0600

    New RR Strafe Test to verify Lateral Multiplier

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR_StrafeTest.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.roadrunner_files.teamcode.drive.DriveConstants;
+import org.firstinspires.ftc.teamcode.roadrunner_files.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.roadrunner_files.teamcode.trajectorysequence.TrajectorySequence;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Config
+@Autonomous(name="ARR Strafe", group="Right")
+
+public class RR_StrafeTest extends LinearOpMode {
+
+    public static double  strafeDistance = 24;
+
+    @Override
+    public void runOpMode() {
+
+        SampleMecanumDrive drive  = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(0, 0, Math.toRadians(90));
+
+        drive.setPoseEstimate(startPose);
+
+        TrajectorySequence traj1  = drive.trajectorySequenceBuilder(startPose)
+                .strafeLeft(strafeDistance)
+                .build();
+
+        TrajectorySequence traj2  = drive.trajectorySequenceBuilder(startPose)
+                .strafeRight(strafeDistance)
+                .build();
+
+        waitForStart();
+        telemetry.addData("Strafing About to Strafe left, then right after 5 second..\n Measure strafe distance to Calculate Lateral Multiplier", strafeDistance);
+        telemetry.update();
+
+        drive.followTrajectorySequence(traj1);
+        sleep(5000);
+        drive.followTrajectorySequence(traj2);
+
+    }
+
+}
